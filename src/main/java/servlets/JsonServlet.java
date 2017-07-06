53689bf48abdc1bad560f25d227348ef799c743e
CoCo-Flyer <694945838@qq.com>
2017-07-06 11:30:59 +0800

    test ajax

---
+++ src/main/java/servlets/JsonServlet.java
@@ -0,0 +1,71 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JsonServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	
+    
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+/**
+    * @see HttpServlet#HttpServlet()
+    */
+   public JsonServlet() {
+       super();
+   }
+
+	
+	
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		
+    	StringBuilder sb = new StringBuilder();
+    	
+    	response.setContentType("text/json");
+    	
+    	sb.append("{\"firstName\": \"Brett\", \"lastName\": \"McLaughlin\"}");    	
+    	
+		response.getWriter().append(sb.toString());
+		//response.flushBuffer();
+
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+		
+	}
+
+}
